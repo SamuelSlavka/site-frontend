fc81aa3e5e8a0961311e90a2ba9183a5df799f8b
samuelslavka <slavka.samuel@gmail.com>
2022-07-24 13:52:11 +0000

    Switch to gitlab

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit save to reload.
+          Homepage 2.
         </p>
       </header>
     </div>
