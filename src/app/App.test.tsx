3f7fd05bc3e8d2490a418b8416b4d98f1dfd000a
samuelslavka <slavka.samuel@gmail.com>
2022-07-31 10:28:52 +0200

    Fixed tests

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App works', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello There!/i);
   expect(linkElement).toBeInTheDocument();
 });
