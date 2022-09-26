a8905090181f54b4d97db581734c86bed2938aee
samuelslavka <slavka.samuel@gmail.com>
2022-09-26 20:48:02 +0200

    build fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import NavItem from "./components/NavItem/NavItem";
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import Time from "./components/Time/Time";
 
