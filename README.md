cf9a4c0bf7bd329ff5b0ca83eb93e21abc10217d
samuelslavka <slavka.samuel@gmail.com>
2022-10-05 18:55:37 +0200

    readme update

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# HEYoouoou
+# Web application
+Application containing links to self-hosted services and scraped menus from lcoal restaurants.
+
+### `yarn test`
+
+### `yarn lint`
+
+### `yarn install`
 
 ### `yarn build`
 
--- src/app/pages/LunchPage/LunchPage.tsx
+++ src/app/pages/LunchPage/LunchPage.tsx
@@ -38,7 +38,7 @@ const LunchPage = () => {
             <section className="m-4 ">
                 <RestaurantSelect />
             </section>
-            <section className="m-4 flex w-100 flex-wrap justify-center">
+            <section className="m-4 flex w-100 flex-wrap justify-center transition-all ease-in-out duration-300">
                 {lunchList}
             </section>
         </div>
