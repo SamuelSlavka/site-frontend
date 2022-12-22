9f9b410b19ca3412590489bac83682ef9e18f05e
samuelslavka <slavka.samuel@gmail.com>
2022-12-23 00:28:47 +0100

    finally did some dashboard layout update

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import NavItem from "./components/NavItem/NavItem";
-import { Link } from "react-router-dom";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import Time from "./components/Time/Time";
 
@@ -7,30 +6,31 @@ import styles from './HomePage.module.scss';
 
 function HomePage() {
     return (
-        <div className="overflow-auto HideScrollbars inline-block relative min-w-full min-h-fit h-full object-cover">
-            <section className="h-fit" data-testid="HomePage" >
-                <div className="text-white h-fit HideScrollbars">
-                    <section className="LinkTopContainer">
-                        <a href="https://gitlab.com/SamuelSlavka/site">
-                            <span className='LinkTop'>{"git repo"}</span>
-                        </a>
-                    </section>
-                    <h1 className={`${styles.HeaderText} pt-8  lg-pt-16 xl:pt-20`}>
-                        Hello there
-                    </h1>
-                    <Time />
-                    <div className="mt-8 mb-6 md:mt-8 lg:mt-16 xl:mt-32 2xl:mt-40 text-center flex w-100 flex-wrap justify-center">
-                        <NavItem name="Game" link="game" icon={solid("gamepad")} />
-                        <NavItem name="Jelly" link="jelly" icon={solid("jar")} />
-                        <NavItem name="Shelf" link="shelf" icon={solid("book")} />
-                        <NavItem name="Next" link="next" icon={solid("cloud")} />
-
-                        <Link to="/lunch">
-                            <NavItem name="Lunch" icon={solid("bowl-food")} />
-                        </Link>
+        <div className="h-full">
+            <div className="LinkTopContainer">
+                <a href="https://gitlab.com/SamuelSlavka/site">
+                    <span className='LinkTop'>{"git repo"}</span>
+                </a>
+            </div>
+            <div className="flex flex-col md:flex-row HideScrollbars relative min-w-full min-h-fit h-full object-cover">
+                <div className={styles.SideBar} data-testid="HomePage" >
+                    <div className="text-white h-fit HideScrollbars">
+                        <h1 className={`${styles.HeaderText} pt-8 lg-pt-16 xl:pt-20`}>
+                            Hello there
+                        </h1>
+                    </div>
+                    <div className="hidden md:block">
+                        <Time/>
                     </div>
                 </div>
-            </section>
+                <div className={`${styles.NavContainer} w-full h-fit flex flex-wrap justify-center pt-2 md:pt-6 pb-4 pr-4`}>
+                    <NavItem name="Flappy bird" route="http://game.sam-sla.net" bg="#1c478c" icon={solid("gamepad")} />
+                    <NavItem name="Jellyfin" route="http://jelly.sam-sla.net" icon={solid("jar")} />
+                    <NavItem name="Audiobookshelf" route="http://shelf.sam-sla.net" icon={solid("book")} />
+                    <NavItem name="NextCloud" route="http://next.sam-sla.net" icon={solid("cloud")} />
+                    <NavItem name="Lunch" link="/lunch" icon={solid("bowl-food")} />
+                </div>
+            </div>
         </div>)
 }
 
--- src/app/pages/HomePage/components/NavItem/NavItem.tsx
+++ src/app/pages/HomePage/components/NavItem/NavItem.tsx
@@ -2,34 +2,39 @@ import React, { FC } from 'react';
 import styles from './NavItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconDefinition } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom'
 
 interface NavItemProps {
-  name: string,
-  link?: string,
-  icon: IconDefinition,
+    name: string,
+    link?: string,
+    route?: string,
+    bg?: string,
+    hover?: string,
+    icon: IconDefinition,
 }
 
 const NavItem: FC<NavItemProps> = (props) => {
-
-  const redirect = (link?: string) => {
-    if(link) {
-      const [prefix, domain] = window.location.href.split('//');
-      const subdomains = domain.split('.');
-      const newHref = `${prefix}//${link}.${subdomains[subdomains.length-2]}.${subdomains[subdomains.length-1]}`;
-      window.location.href = newHref;
+    const navigate = useNavigate();
+    const redirect = (props: NavItemProps) => {
+        if (props.route) {
+            window.location.href = props.route;
+        } else if (props.link) {
+            navigate(props.link);
+        }
     }
-  }
 
-  return (
-    <button onClick={() => redirect(props.link)} className={`${styles.card} text-2xl p-2 m-2 md:p-4 md:m-4 lg:m-6 rounded relative hover:text-dark`} data-testid="NavItem">
-      <section className={`${styles.cardContent} p-2 md:p-4 lg:px-6 lg:py-4 relative z-20`}>
-        <FontAwesomeIcon icon={props.icon} />
-        <span className="px-4">
-          {props.name}
-        </span>
-      </section>
-    </button>
-  );
+    return (
+        <div className="m-auto mx-3 self-center">
+            <button style={{ backgroundColor: props.bg}} onClick={() => redirect(props)} className={`${styles.card} text-2xl p-2 m-2 md:p-4 md:m-4 lg:m-6 rounded relative`} data-testid="NavItem">
+                <div className='p-2 md:p-4 lg:px-6 lg:py-4 relative z-20'>
+                    <FontAwesomeIcon icon={props.icon} />
+                    <span className="px-4">
+                        {props.name}
+                    </span>
+                </div>
+            </button>
+        </div>
+    );
 };
 
 export default NavItem;
