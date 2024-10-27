eb9fceb40ff341e832427a6f641db25163ae69a1
sam <slavka.samuel@gmail.com>
2024-10-27 15:58:00 +0100

    upgrade

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -23,6 +23,7 @@ export class DashboardPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(new MeasurementActions.GetLatest());
+    console.log(this.keycloakService.isLoggedIn());
   }
 
   logout() {
