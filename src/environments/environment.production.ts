b3de9dd55548e49f1c452429bd72e3cdd8b37f28
sam <slavka.samuel@gmail.com>
2025-01-24 17:38:00 +0100

    domain migration

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appName: 'site-frontend',
-  serverUrl: 'https://api.sam-sla.net/api/v1/',
+  serverUrl: 'https://api.samsla.org/api/v1/',
   production: true,
   i18nPrefix: '',
   defaultLanguage: 'en',
@@ -8,7 +8,7 @@ export const environment = {
   supportedLanguages: ['en'],
   keycloak: {
     // Url of the Identity Provider
-    issuer: 'https://keycloak.sam-sla.net',
+    issuer: 'https://keycloak.samsla.org',
     // Realm
     realm: 'site',
     clientId: 'site-frontend',
